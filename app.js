182bd9fe715ae189081e640c434890f0a3154c08
Deepak Kumar Jha <deepak.jhaaa@gmail.com>
2017-12-28 00:14:30 +0530

    commit/.

--- app.js
+++ app.js
@@ -38,12 +38,17 @@ var routes_C_1_0 = require('./routes/C.1.0/index');
 var routes_C_1_1 = require('./routes/C.1.1/index');
 var routes_U_1_0 = require('./routes/U.1.0/index');
 
+// Base routes for: All the secured connection request
+var routes_S_1_0 = require('./routes/S.1.0/index');
+
 //Multiple routes structure for different projects/versions
 app.use('/', routes);
 app.use('/v1', routes_U_1_0);
 app.use('/v2', routes_C_1_0);
 app.use('/v3', routes_C_1_1);
 
+// All the secured connection request will pass through this pipeline
+app.use('/s1', routes_S_1_0);
 
 //Catch 404 and forward to error handler
 app.use(function (req, res, next) {
