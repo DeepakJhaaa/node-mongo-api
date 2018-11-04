273692deb1219df4a76b5292b1dd41df4199ce33
Deepak Jha <deepak.jhaaa@gmail.com>
2018-11-04 21:30:42 +0530

    working on features-001

---
+++ config/cors.js
@@ -0,0 +1,7 @@
+var allowedOrigins = [
+    'https://ngx-todo.dkjha.com',
+    'https://ngx-chat.dkjha.com',
+    'https://ngx-user.dkjha.com',
+    'https://ngx-team.dkjha.com',
+    'http://localhost:4200'
+];
